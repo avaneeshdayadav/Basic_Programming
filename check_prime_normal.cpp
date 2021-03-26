fcc09b5a9c4c6f49fd31251ad6c2adf5c9bcdea6
avaneeshdayadav <60056707+avaneeshdayadav@users.noreply.github.com>
2021-03-26 13:22:27 +0530

    Update check_prime_normal.cpp

--- check_prime_normal.cpp
+++ check_prime_normal.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+void solve()
+{
+  long long n;
+  bool flag=1;
+  cin>>n;
+  
+  for(long long i=2; i<=sqrt(n); i++)
+  {
+    if(n % i == 0)
+    {
+      flag = 0;
+      break;
+    }
+  }
+  
+  
+  if(flag == 1)
+    cout<<"Prime"<<endl;
+  else
+    cout<<"Not Prime"<<endl;
+    
+}
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--)
+        solve();
+}
