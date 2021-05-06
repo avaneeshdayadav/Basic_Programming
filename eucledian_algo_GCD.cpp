a28032cc8edab207b8fd1135e6e8d3ed8498045d
avaneeshdayadav <60056707+avaneeshdayadav@users.noreply.github.com>
2021-05-06 11:17:24 +0530

    Create eucledian_algo_GCD.cpp

---
+++ eucledian_algo_GCD.cpp
@@ -0,0 +1,51 @@
+
+// Finding GCD by Eucledian Algorithm both by using - and % operator.
+
+#include <iostream>
+using namespace std;
+
+// Less efficient than using % operator
+long long GCD_by_substraction(long long m,long long n) //reccursive function
+{
+    // Everything divides 0
+    if (m == 0)
+       return n;
+    if (n == 0)
+       return m;
+    
+    // Base condition
+    if(m==n)
+        return m;
+        
+    // call GCD_by_substraction(diff_of_m&n,smallest_of_m&n);
+    if(m>n)
+        GCD_by_substraction(m-n,n);
+    else
+        GCD_by_substraction(n-m,m);
+}
+
+
+// More efficient than using - operator.
+long long GCD_by_modulo(long long m, long long n) // reccursive function
+{
+    // Everything divides 0
+    if (m == 0)
+       return n;
+    if (n == 0)
+       return m;
+       
+    // Base condition
+    if(m==0)
+        return n;
+    GCD_by_modulo(n%m,m);
+}
+
+int main()
+{
+    long long m,n;
+    cin>>m>>n;
+    cout<<GCD_by_modulo(m,n)<<endl;
+    cout<<GCD_by_substraction(m,n)<<endl;
+    
+    
+}
