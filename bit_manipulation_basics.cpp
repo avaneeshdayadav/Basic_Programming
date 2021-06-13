844880bb3f610a70cd7be6877451135dd592d98f
avaneeshdayadav <60056707+avaneeshdayadav@users.noreply.github.com>
2021-06-13 06:38:55 +0530

    Update bit_manipulation_basics.cpp

--- bit_manipulation_basics.cpp
+++ bit_manipulation_basics.cpp
@@ -1,1 +1,39 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n,i;
+    cin>>n;
+    cin>>i;
+    
+    
+    /*
+        Here if n = 10 then,
+        binary form : 1010
+        index       : 3210
+    */
+    
+	// get ith index bit
+	
+	if(n & (1<<i))
+	    cout<<"ith bit is 1"<<endl;
+    else
+	    cout<<"ith bit is 0"<<endl;
+	    
+	    
+	    
+	// set ith index bit
+	
+    cout<<"After setting ith bit, no is "<<(n | (1<<i))<<endl;
+    
+    
+    
+    // unset/clear ith index bit
+	
+    cout<<"After clearing ith bit, no is "<<(n & ~(1<<i))<<endl;
+
+    
+	return 0;
+}
