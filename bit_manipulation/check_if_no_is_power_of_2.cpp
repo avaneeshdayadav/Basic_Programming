f2e76bd8d2c385b2d003ebeef673fd19513500eb
avaneeshdayadav <60056707+avaneeshdayadav@users.noreply.github.com>
2021-06-13 07:47:06 +0530

    Update check_if_no_is_power_of_2.cpp

--- bit_manipulation/check_if_no_is_power_of_2.cpp
+++ bit_manipulation/check_if_no_is_power_of_2.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    
+    /*
+        Imp Note Points :
+        1) If a number is power of 2 then it has only one set bit which is leftmost bit.
+        2)
+    */
+	return 0;
+}
