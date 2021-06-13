f067f6f6ad9eba316cca9c773119b57a1482856e
avaneeshdayadav <60056707+avaneeshdayadav@users.noreply.github.com>
2021-06-13 08:58:41 +0530

    Update find_unique_no.cpp

--- bit_manipulation/find_unique_no.cpp
+++ bit_manipulation/find_unique_no.cpp
@@ -5,23 +5,26 @@ int main() {
     
     /*
     
-    Problem Statement : Given a array containing only one unique no and other all no's r repeated even no. of times.
+    Problem Statement 1 : Given a array containing only one unique no and other all no's r repeated even no. of times.
+                        Find the unique no.
+                        
+    Problem Statement 2 : Given a array containing only two unique no's and other all no's r repeated even no. of times.
                         Find the unique no.
     
     Concept : Same no's when xored gives zero.
         
     */
     
-    int arr[7] = {2,4,6,3,4,6,2},size=0,temp=0;
+    int arr1[7] = {2,4,6,3,4,6,2},size1=0,temp1=0;
     
-    size = sizeof(arr)/sizeof(int);
+    size1 = sizeof(arr1)/sizeof(int);
     
-    for(int i=0;i<size;i++)
+    for(int i=0;i<size1;i++)
     {
-        temp = (temp xor arr[i]);
+        temp1 = (temp1 xor arr1[i]);
     }
     
-    cout<<"Unique number is "<<temp<<endl;
+    cout<<"Unique number is "<<temp1<<endl;
 
     return 0;
 }
